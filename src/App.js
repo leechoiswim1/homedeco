3d7607a67739dbf4e5570f0d0481c66e6278349c
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-01-31 23:26:37 +0900

    Add Api 연결 및 useState 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from "react";
-
+import React, { useEffect, useState } from 'react';
+import { getData } from './service/Api';
 const App = () => {
+  const [items, setItems] = useState({ id: '', imageUrl: '', productList: '' });
+
+  useEffect(() => {
+    getData().then(res => {
+      setItems({ id: res.id, imageUrl: res.imageUrl, productList: res.productList });
+    });
+  }, []);
   return <div></div>;
 };
 
