922abf0a0d0169e4426ef04d6b1d5a7dad5538e2
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-02 06:06:07 +0900

    Add 위치에 따라 상품 설명 박스 top,left 변경

--- src/Components/ProductMain.jsx
+++ src/Components/ProductMain.jsx
@@ -1,19 +1,63 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState, useRef } from 'react';
+import styled, { css } from 'styled-components';
 import { SlideMenu } from './';
-const ProductMain = ({ imageUrl, productList, newX }) => {
+
+const ProductMain = ({ imageUrl, productList }) => {
+  const [selectProduct, setSelectProduct] = useState('');
+  const [imageHeight, setImageHeight] = useState('');
+  const [imageWidth, setImageWidth] = useState('');
+  const imageRef = useRef();
+  const handleSelect = productId => {
+    productId === selectProduct ? setSelectProduct(0) : setSelectProduct(productId);
+  };
+  useEffect(() => {
+    setImageHeight(imageRef.current.height);
+    setImageWidth(imageRef.current.width);
+  }, []);
+  console.log(imageWidth);
   return (
     <Wrapper>
-      <ProductImage src={imageUrl} alt="productImage" />
+      <ProductImage
+        ref={imageRef}
+        src={imageUrl}
+        alt="productImage"
+        onClick={() => handleSelect(0)}
+      />
       {productList &&
         productList.map(product => {
+          const newPointX = product.pointX * 1.6;
+          const newPointY = product.pointY * 1.65;
+          const BottomBox = imageHeight && imageHeight / 2 - newPointX < 0 ? true : false;
+          const leftBox = imageWidth - newPointY < 220 ? true : false;
+
           return (
-            <Magnify key={product.productId} pointX={product.pointX} pointY={product.pointY}>
-              <MagIcon src="//cdn.ggumim.co.kr/storage/20211029145238AlZrQ41xtg.png" />
-            </Magnify>
+            <div key={product.productId}>
+              <Magnify
+                newPointX={newPointX}
+                newPointY={newPointY}
+                onClick={() => handleSelect(product.productId)}
+              >
+                <MagIcon
+                  src={
+                    selectProduct === product.productId
+                      ? 'https://cdn.ggumim.co.kr/storage/20211029145330GwwumnWNSs.png'
+                      : '//cdn.ggumim.co.kr/storage/20211029145238AlZrQ41xtg.png'
+                  }
+                />
+                <DescBox
+                  BottomBox={BottomBox}
+                  leftBox={leftBox}
+                  boxDisplay={selectProduct === product.productId ? 'block' : 'none'}
+                ></DescBox>
+              </Magnify>
+            </div>
           );
         })}
-      <SlideMenu productList={productList} newX={newX} />
+      <SlideMenu
+        productList={productList}
+        handleSelect={handleSelect}
+        selectProduct={selectProduct}
+      />
     </Wrapper>
   );
 };
@@ -27,10 +71,11 @@ const Wrapper = styled.section`
 const ProductImage = styled.img`
   width: 800px;
 `;
+
 const Magnify = styled.div`
   position: absolute;
-  top: ${props => props.pointX && props.pointX * 1.6}px;
-  left: ${props => props.pointY && props.pointY * 1.65}px;
+  top: ${props => props.newPointX && props.newPointX}px;
+  left: ${props => props.newPointY && props.newPointY}px;
   width: 40px;
   height: 40px;
 `;
@@ -38,6 +83,38 @@ const Magnify = styled.div`
 const MagIcon = styled.img`
   width: 32px;
   height: 32px;
+`;
+
+const DescBox = styled.span`
+  box-sizing: border-box;
+  z-index: 1000;
+  display: ${props => props.boxDisplay && props.boxDisplay};
+  align-items: center;
+  position: absolute;
+  background-color: rgba(255, 255, 255, 0.95);
+  width: 220px;
+  height: 86px;
+  padding: 8px 0 8px 8px;
+  margin-top: 16px;
+  border-radius: 7px;
+  box-shadow: 3px 3px 8px 0 rgb(0 0 0 / 20%);
+  font-size: 14px;
+  color: #4a4a4a;
+  ${props =>
+    props.BottomBox && props.BottomBox
+      ? css`
+          top: unset;
+          bottom: 52px;
+        `
+      : css`
+          top: 28px;
+          left: -20px;
+        `}
+  ${props =>
+    props.leftBox &&
+    css`
+      left: -160px;
+    `}
 `;
 
 export default ProductMain;
--- src/Components/SlideItem.jsx
+++ src/Components/SlideItem.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
-const SlideItem = ({ id, imageUrl, discountRate }) => {
+import React, { useState, useEffect } from 'react';
+import styled, { css } from 'styled-components';
+const SlideItem = ({ id, imageUrl, discountRate, handleSelect, selectProduct }) => {
+  const [selectedItem, setSelectedItem] = useState(false);
+  useEffect(() => {
+    setSelectedItem(selectProduct === id ? true : false);
+  }, [selectProduct]);
   return (
-    <SlideItemWrapper>
-      <SlideItemImage id={id} imageUrl={imageUrl} />
+    <SlideItemWrapper selectedItem={selectedItem}>
+      <SlideItemImage
+        id={id}
+        imageUrl={imageUrl}
+        selectedItem={selectedItem}
+        onClick={() => {
+          handleSelect(id);
+        }}
+      />
       <DisCountBox></DisCountBox>
     </SlideItemWrapper>
   );
@@ -17,13 +28,21 @@ const SlideItemWrapper = styled.section`
   position: relative;
   transition-property: transform;
   flex-shrink: 0;
+  ${props =>
+    props.selectedItem &&
+    css`
+      background: linear-gradient(163.54deg, #ff659e 8.22%, #f56b30 94.1%);
+      margin: 26px 4px;
+      padding: 2px;
+      border-radius: 18px;
+    `}
 `;
 const SlideItemImage = styled.div`
   position: relative;
   width: 106px;
   height: 106px;
   border-radius: 16px;
-  border: 0.5px solid #aaafb9;
+  border: ${props => (props.selectedItem ? 'none' : '0.5px solid #aaafb9')};
   user-select: none;
   cursor: pointer;
   background-position: center center;
