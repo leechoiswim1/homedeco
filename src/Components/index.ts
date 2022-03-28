ef92f37faef51345f6c05273173c8b3547ffe84a
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-28 23:57:38 +0900

    :art: 폴더 구조변경으로 인해 컴포넌트 index 내용 수정

---
+++ src/Components/index.ts
@@ -0,0 +1,5 @@
+export { default as ContentHeader } from './ContentHeader';
+export { default as ProductMain } from './ProductMain/ProductMain';
+export { default as SlideMenu } from './SlideMenu/SlideMenu';
+export { default as SlideItem } from './SlideItem';
+export { default as ToolTip } from './ToolTip';
