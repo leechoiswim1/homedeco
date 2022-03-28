25afa85315b991de692c3159e706650b13332231
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-29 01:26:52 +0900

    :art: theme 추가 및 글로벌 스타일 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'styles/GlobalStyle';
+import theme from 'styles/theme';
+import App from 'page/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+const colors = {
+  white: '#ffffff',
+  backgroundWhite: '#fffffff2',
+  lightGray: '#afafaf',
+  gray: '#646464',
+  descGray: '#4a4a4a',
+  descBlack: '#333c45',
+  expectGray: ' #898f94',
+  discountBlack: '#181d1f',
+  tomato: '#ff585d',
+  pink: '#ff659e',
+  orange: '#f56b30',
+  borderGray: '#aaafb9',
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
