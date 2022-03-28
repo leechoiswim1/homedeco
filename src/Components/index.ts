5630b8ccc1a18c5f695a0303947b31cc54832fa7
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-29 01:27:58 +0900

    :art: 컴포넌트내 폴더구조 변경

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,5 +1,5 @@
-export { default as ContentHeader } from './ContentHeader';
+export { default as ContentHeader } from './ContentHeader/ContentHeader';
 export { default as ProductMain } from './ProductMain/ProductMain';
 export { default as SlideMenu } from './SlideMenu/SlideMenu';
-export { default as SlideItem } from './SlideItem';
-export { default as ToolTip } from './ToolTip';
+export { default as SlideItem } from './SlideItem/SlideItem';
+export { default as ToolTip } from './ToolTip/ToolTip';
