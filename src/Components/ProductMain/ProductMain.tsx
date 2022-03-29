c4b02b667e687e4947e7edcc7ed014c4026c2854
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-29 13:47:01 +0900

    :zap: react.memo사용을 통한 최적화

--- src/Components/ProductMain/ProductMain.tsx
+++ src/Components/ProductMain/ProductMain.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useRef, useCallback, ReactElement } from 'react';
 import { SlideMenu, ToolTip } from 'Components';
 import * as S from './ProductMain.style';
 import { ProductMainProps } from './ProductMain.type';
@@ -23,51 +23,55 @@ const ProductMain = ({ imageUrl, productLists }: ProductMainProps) => {
     }
   }, [selectProduct]);
 
-  return (
-    <S.ProductWrapper>
-      <S.ProductImage
-        ref={imageRef}
-        src={imageUrl}
-        alt="productImage"
-        onClick={() => handleSelect(null)}
-      />
-      {productLists &&
-        productLists.map(product => {
-          const newPointX = product.pointX * 1.67;
-          const newPointY = product.pointY * 1.67;
-          const BottomBox = imageHeight && imageHeight / 2 - newPointX < 0 ? true : false;
-          const leftBox = imageWidth && imageWidth - newPointY < imageWidth / 2 ? true : false;
-          return (
-            <div key={product.productId}>
-              <S.Magnify
-                newPointX={newPointX}
-                newPointY={newPointY}
-                onClick={() => handleSelect(product.productId)}
-              >
-                <S.MagIcon
-                  src={
-                    selectProduct === product.productId
-                      ? 'https://cdn.ggumim.co.kr/storage/20211029145330GwwumnWNSs.png'
-                      : '//cdn.ggumim.co.kr/storage/20211029145238AlZrQ41xtg.png'
-                  }
-                />
-                <ToolTip
-                  BottomBox={BottomBox}
-                  leftBox={leftBox}
-                  boxDisplay={selectProduct === product.productId}
-                  product={product}
-                />
-              </S.Magnify>
-            </div>
-          );
-        })}
-      <SlideMenu
-        productLists={productLists}
-        handleSelect={handleSelect}
-        selectProduct={selectProduct}
-      />
-    </S.ProductWrapper>
+  const Products = React.memo(
+    (): ReactElement => (
+      <S.ProductWrapper>
+        <S.ProductImage
+          ref={imageRef}
+          src={imageUrl}
+          alt="productImage"
+          onClick={() => handleSelect(null)}
+        />
+        {productLists &&
+          productLists.map(product => {
+            const newPointX = product.pointX * 1.67;
+            const newPointY = product.pointY * 1.67;
+            const BottomBox = imageHeight && imageHeight / 2 - newPointX < 0 ? true : false;
+            const leftBox = imageWidth && imageWidth - newPointY < imageWidth / 2 ? true : false;
+            return (
+              <div key={product.productId}>
+                <S.Magnify
+                  newPointX={newPointX}
+                  newPointY={newPointY}
+                  onClick={() => handleSelect(product.productId)}
+                >
+                  <S.MagIcon
+                    src={
+                      selectProduct === product.productId
+                        ? 'https://cdn.ggumim.co.kr/storage/20211029145330GwwumnWNSs.png'
+                        : '//cdn.ggumim.co.kr/storage/20211029145238AlZrQ41xtg.png'
+                    }
+                  />
+                  <ToolTip
+                    BottomBox={BottomBox}
+                    leftBox={leftBox}
+                    boxDisplay={selectProduct === product.productId}
+                    product={product}
+                  />
+                </S.Magnify>
+              </div>
+            );
+          })}
+        <SlideMenu
+          productLists={productLists}
+          handleSelect={handleSelect}
+          selectProduct={selectProduct}
+        />
+      </S.ProductWrapper>
+    )
   );
+
+  return <Products />;
 };
 
 export default ProductMain;
